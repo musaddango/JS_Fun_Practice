c43b6efae36fd31c58fc08aed83e1bcac426982f
Musa Dango <musaddangoWgmail.com>
2024-07-12 11:42:15 +0100

    musaddango

--- Solutions/musaddango_solution.js
+++ Solutions/musaddango_solution.js
@@ -1,95 +1,103 @@
 const identity = (n) => n;
 
-const addb = (a,b) => a+b;
+const addb = (a, b) => a + b;
 
-const subb = (a,b) => a > b ? a-b : b-a;
+const subb = (a, b) => a > b ? a - b : b - a;
 
-const mulb = (a,b) => a * b;
+const mulb = (a, b) => a * b;
 
-const minb = (a,b) => a < b ? a : b;
+const minb = (a, b) => a < b ? a : b;
 
-const maxb = (a,b) => a > b ? a : b;
+const maxb = (a, b) => a > b ? a : b;
 
-function add (...nums){
+function add(...nums) {
     const array = [...nums];
     let sum = 0;
-    for(let i = 0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         sum += array[i]
     }
     return sum;
 };
 
 
-function mul(...nums){
+function mul(...nums) {
     const array = [...nums];
     let mult = 1;
-    for (let i = 0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         mult *= array[i];
     }
     return mult;
 }
 
-function min(...nums){
+function min(...nums) {
     const array = [...nums];
     let smallest = array[0];
-    for (let i=0; i < array.length; i++){
-        if(smallest > array[i]){
+    for (let i = 0; i < array.length; i++) {
+        if (smallest > array[i]) {
             smallest = array[i];
         }
     }
     return smallest;
 }
 
-function max(...nums){
+function max(...nums) {
     const array = [...nums];
     let maxNum = array[0];
-    for(let i = 0; i<array.length; i++){
-        if(array[i] > maxNum){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maxNum) {
             maxNum = array[i];
         }
     }
     return maxNum;
 }
 
-function addRecurse(...nums){
-    if(nums.length === 0) return 0;
+function addRecurse(...nums) {
+    if (nums.length === 0) return 0;
 
     let newArray = nums.slice(1)
     return nums[0] + addRecurse(newArray);
 }
 
-function mulRecurse(...nums){
-    if(nums.length === 0) return 1;
+function mulRecurse(...nums) {
+    if (nums.length === 0) return 1;
 
     let newArray = nums.slice(1);
     return nums[0] * mulRecurse(...newArray);
 }
 
-function minRecurse(...nums){
+function minRecurse(...nums) {
     // Base case
-    if(nums.length === 1){
+    if (nums.length === 1) {
         return nums[0];
     }
     const lastSmallest = minRecurse(...nums.slice(1));
     return nums[0] < lastSmallest ? nums[0] : lastSmallest;
 }
 
-function maxRecurse(...nums){
+function maxRecurse(...nums) {
     //Base case
-    if(nums.length === 1){
+    if (nums.length === 1) {
         return [0];
     }
     const lastBiggest = maxRecurse(...nums.slice(1));
     return nums[0] > lastBiggest ? nums[0] : lastBiggest;
 }
 
-function not(func){
-    if(!(func instanceof Function) || typeof func() !== "number"){
+function not(func) {
+    if (!(func instanceof Function) || typeof func() !== "number") {
         return "Invalid argument.";
     }
 
     return func() * -1;
 }
+
+function acc(func, initial) {
+
+    return function (...args) {
+        return func(args, initial);
+    }
+}
+
 
 
 
@@ -110,7 +118,7 @@ module.exports = {
     minRecurse,
     maxRecurse,
     not,
-    // acc,
+    acc,
     // accPartial,
     // accRecurse,
     // fill,
